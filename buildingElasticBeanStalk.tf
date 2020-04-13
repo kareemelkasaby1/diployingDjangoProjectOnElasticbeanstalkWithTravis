a5f561b139ebef308afffa60d252adb6fef02787
Kareem Elkasaby <elkasaby@localhost.localdomain>
2020-04-13 04:08:10 +0200

    ff

--- buildingElasticBeanStalk.tf
+++ buildingElasticBeanStalk.tf
@@ -3,8 +3,8 @@ resource "aws_elastic_beanstalk_application" "django-project" {
   description = "django-project"
 }
 
-resource "aws_elastic_beanstalk_environment" "django-project-env" {
-  name                = "django-project-env"
+resource "aws_elastic_beanstalk_environment" "DjangoProject-env" {
+  name                = "DjangoProject-env"
   application         = "${aws_elastic_beanstalk_application.django-project.name}"
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.20.0 running Multi-container Docker 18.09.9-ce (Generic)"
   wait_for_ready_timeout = "120m"
